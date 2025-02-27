768c704f2d655f63f4a11b145eb55ec87f425353
botij0 <javier.gonzalez.soldado@gmail.com>
2025-02-27 12:02:10 +0100

    feat: basic block

---
+++ src/JaChain.java
@@ -0,0 +1,13 @@
+public class JaChain {
+
+    public static void main(String[] args){
+        Block genesisBlock = new Block("First Block", "0");
+        System.out.println("Hash for Block 1: " + genesisBlock.hash);
+
+        Block secondBlock = new Block("Second Block", genesisBlock.hash);
+        System.out.println("Hash for Block 2: " + secondBlock.hash);
+
+        Block thirdBlock = new Block("Third Block", secondBlock.hash);
+        System.out.println("Hash for Block 3: " + thirdBlock.hash);
+    }
+}
